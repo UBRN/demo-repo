c0dc4b5660eb8c58c6fcddfb25188931ea56d8ec
utku <57094489+UBRN@users.noreply.github.com>
2023-04-12 13:40:21 +0300

    Update chat.py

---
+++ chat.py
@@ -0,0 +1,5 @@
+Selam, müsait olduğunda dün akşam konuştuğumuz şu konuyu araştırabilir misin
+abi dünkü notlarım arasında şu da var:
+- AZ3 lere config'ler doğru gitmiyor mu Windows servislere paket çıkarken?
+başta config file'ları elle taşıdık ya
+onu da isterseniz siz bir kontrol edin, ekip yine sürüm çıkarsa AZ3'teki eski kalmasın
